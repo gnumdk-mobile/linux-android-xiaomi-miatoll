65c060057b1ec4559e5dedc22dca83985ab77adc
Joel Fernandes <joelaf@google.com>
2018-06-30 17:17:24 +0000

    sched/fair: Skip frequency updates if CPU about to idle
    If CPU is about to idle, prevent a frequency update. With the number of
    schedutil governor wake ups are reduced by more than half on a test
    playing bluetooth audio.
    
    Test: sugov wake ups drop by more than half when playing music with
    screen off (476 / 1092)
    
    Bug: 64689959
    
    Change-Id: I400026557b4134c0ac77f51c79610a96eb985b4a
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    (cherry picked from commit 6b8a37d0a0ab3872e8751bdbc2f4936060d2e245)
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3740,7 +3740,7 @@ static inline void update_load_avg(struct sched_entity *se, int flags)
 	if (se->avg.last_update_time && !(flags & SKIP_AGE_LOAD))
 		__update_load_avg_se(now, cpu, cfs_rq, se);
 
-	decayed  = update_cfs_rq_load_avg(now, cfs_rq, true);
+	decayed  = update_cfs_rq_load_avg(now, cfs_rq, !(flags & SKIP_CPUFREQ));
 	decayed |= propagate_entity_load_avg(se);
 
 	if (decayed && (flags & UPDATE_TG))
