42fd4d62c060a999069aa6bc0b1d5b63c63b3c52
Yunyun Cao <yunyunc@codeaurora.org>
2020-01-13 11:26:38 +0800

    msm: ais: reduce error message printed during initialization
    Failure is expected during vfe rd bus init and ife dsp clk as
    they are not used on automotive platform. Change log level
    to CAM_INFO accordingly.
    
    Change-Id: I5a01675977dedf210c1c82e851a9bb3100db1d4e
    Signed-off-by: Yunyun Cao <yunyunc@codeaurora.org>

--- drivers/media/platform/msm/ais/cam_sync/cam_sync.c
+++ drivers/media/platform/msm/ais/cam_sync/cam_sync.c
@@ -840,7 +840,7 @@ static int cam_sync_open(struct file *filep)
 
 	/* return if already initialized before */
 	if (sync_dev->open_cnt > 1) {
-		CAM_ERR(CAM_SYNC, "Already opened", rc);
+		CAM_WARN(CAM_SYNC, "Already opened", rc);
 		goto end;
 	}
 
