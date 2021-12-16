d2a7bab81ebb065409db4e8debe966259f42f510
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2021-12-16 17:07:17 +0200

    packaging: Update kernel version

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -8,7 +8,7 @@
 VARIANT = android
 
 # Kernel base version
-KERNEL_BASE_VERSION = 4.14-254
+KERNEL_BASE_VERSION = 4.14-257
 
 # The kernel cmdline to use
 KERNEL_BOOTIMAGE_CMDLINE = console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa88000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 loop.max_part=7 buildvariant=user console=tty0 datapart=/dev/sda18
