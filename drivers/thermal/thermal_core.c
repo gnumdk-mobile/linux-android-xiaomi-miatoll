7e435bf76ebba7883a0d9b052c7bcfac54d57bc3
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-06-30 10:51:49 +0200

    thermal: queue work on power efficient wq

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1789,7 +1789,7 @@ static int __init thermal_init(void)
 
 	mutex_init(&poweroff_lock);
 	thermal_passive_wq = alloc_workqueue("thermal_passive_wq",
-						WQ_UNBOUND | WQ_FREEZABLE,
+						WQ_POWER_EFFICIENT | WQ_FREEZABLE,
 						THERMAL_MAX_ACTIVE);
 	if (!thermal_passive_wq) {
 		result = -ENOMEM;
